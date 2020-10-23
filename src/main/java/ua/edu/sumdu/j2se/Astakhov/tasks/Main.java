46d353b93021a354ccbc3994408f605e70d8c4e0
Astakhov Dmitryy <adima4347@gmail.com>
2020-10-23 18:30:28 +0300

    Initial commit

---
+++ src/main/java/ua/edu/sumdu/j2se/Astakhov/tasks/Main.java
@@ -0,0 +1,11 @@
+package ua.edu.sumdu.j2se.Astakhov.tasks;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		int [] anArray = {11, 12, 15, 16, 17};
+		System.out.println(anArray[0]);
+
+	}
+}
