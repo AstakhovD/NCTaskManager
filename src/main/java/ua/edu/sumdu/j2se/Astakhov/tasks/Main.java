be35c42f457e10754ceb8d3fde6bb7a172705e0d
Astakhov Dmitryy <adima4347@gmail.com>
2020-10-23 18:46:17 +0300

    Practice1: Task

--- src/main/java/ua/edu/sumdu/j2se/Astakhov/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/Astakhov/tasks/Main.java
@@ -2,10 +2,7 @@ package ua.edu.sumdu.j2se.Astakhov.tasks;
 
 public class Main {
 
-
 	public static void main(String[] args) {
-		int [] anArray = {11, 12, 15, 16, 17};
-		System.out.println(anArray[0]);
-
+		System.out.println("Hello World!");
 	}
 }
---
+++ src/main/java/ua/edu/sumdu/j2se/Astakhov/tasks/Task.java
@@ -0,0 +1,221 @@
+package ua.edu.sumdu.j2se.Astakhov.tasks;
+
+/***
+ * Class Task
+ *
+ * @author Астахов Дмитрій
+ *
+ */
+
+public class Task {
+
+    private String title;
+    private int time;
+    private int start;
+    private int end;
+    private int interval;
+    private boolean active;
+    private boolean repeated;
+
+    /***
+     * Constructor Task
+     *
+     * @param title of type String
+     * @param time of type int
+     */
+
+    public Task(String title, int time) {
+        this.title = title;
+        this.time = time;
+    }
+
+    /***
+     * Constructor Task
+     *
+     * @param title of type String
+     * @param start of type int
+     * @param end of type int
+     * @param interval of type int
+     */
+
+    public Task(String title, int start, int end, int interval) {
+        this.title = title;
+        this.start = start;
+        this.end = end;
+        this.interval = interval;
+        repeated = true;
+    }
+
+    
+    /***
+     * Method getTitle returns the title of this object.
+     *
+     * @return the title of this object.
+     */
+
+    public String getTitle() {
+        return title;
+    }
+
+    /***
+     * Method setTitle sets the title of this object.
+     *
+     * @param title - the title of this object.
+     */
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /***
+     *Method isActive returns the active of this object.
+     *
+     * @return the active of this object.
+     */
+
+    public boolean isActive() {
+        return active;
+    }
+
+    /***
+     * Method setActive sets the active of this object.
+     *
+     * @param active - the active of this object.
+     */
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    /***
+     * Method getTime returns the execution time of the Task.
+     * If the task repeats, the repeat start time is returned.
+     *
+     * @return the time of this object.
+     */
+
+    public int getTime() {
+        if (repeated) {
+            return start;
+        }
+        return time;
+    }
+
+    /***
+     * Method setTime sets the time for the task to run. If the task is repeated , then it will be converted
+     * to a non-recurring.
+     *
+     * @param time - the time of this object.
+     */
+
+    public void setTime(int time) {
+        if (repeated) {
+            repeated = false;
+        }
+        this.time = time;
+    }
+
+    /***
+     * Method getStartTime returns the start time of the task.
+     * If the task is not repeated, then returns the task completion time.
+     *
+     * @return the start of this object
+     */
+
+    public int getStartTime() {
+        if (!repeated) {
+            return time;
+        }
+        return start;
+    }
+
+    /***
+     * Method getEndTime returns the end time of the task.
+     * If the task is not repeated, then returns the time the task completion time.
+     *
+     * @return the end of this object.
+     */
+
+    public int getEndTime() {
+        if (!repeated) {
+            return time;
+        }
+        return end;
+    }
+
+    /***
+     * Method getRepeatInterval returns the time interval between executions of tasks, which repeats.
+     * If the task is not repeated, then returns 0.
+     *
+     * @return the interval of this object.
+     */
+
+    public int getRepeatInterval() {
+        if (!repeated) {
+            return 0;
+        }
+        return interval;
+    }
+
+    /***
+     * Method setTime sets repeated, if the task does not repeat.
+     *
+     * @param start of type int
+     * @param end of type int
+     * @param interval of type int
+     */
+
+    public void setTime(int start, int end, int interval) {
+        if (!repeated) {
+            repeated = true;
+        }
+    }
+
+    /***
+     * Method isRepeated returns the repeated of this object.
+     *
+     * @return the repeated of this object.
+     */
+
+    public boolean isRepeated() {
+        return repeated;
+    }
+
+    /***
+     * Method nextTimeAfter returns the next task execution time.
+     *
+     * @param current of type int
+     * @return the current of this object
+     */
+
+    public int nextTimeAfter(int current) {
+        if (isActive()) {
+            if (getStartTime() > current) {
+                return getStartTime();
+            } else if ((getEndTime() <= current)) {
+                return -1;
+            }
+            if (current + getRepeatInterval() > getEndTime()) {
+                return -1;
+            }
+            if (getRepeatInterval() > 0) {
+                if (getStartTime() >= getEndTime()) {
+                    return -1;
+                }
+                if (getStartTime() <= current) {
+                    int time = getStartTime();
+                    while (time <= current) {
+                        time += getRepeatInterval();
+                    }
+                    return time;
+                }
+                if (current >= getEndTime()) {
+                    return -1;
+                }
+            }
+        } else if (!isActive()) {
+            return -1;
+        }
+        return current;
+    }
+}
