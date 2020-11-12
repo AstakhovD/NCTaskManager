a0115ec62a672962a7a9abcb97c4cc42b5972532
Astakhov Dmitryy <adima4347@gmail.com>
2020-11-12 10:38:08 +0300

    Practice4

--- src/main/java/ua/edu/sumdu/j2se/astakhov/tasks/TaskListFactory.java
+++ src/main/java/ua/edu/sumdu/j2se/astakhov/tasks/TaskListFactory.java
@@ -2,10 +2,10 @@ package ua.edu.sumdu.j2se.astakhov.tasks;
 
 public class TaskListFactory {
     public static AbstractTaskList createTaskList(ListTypes.types type) {
-        if (type.equals(ListTypes.types.ARRAY)) {
+        if (type == (ListTypes.types.ARRAY)) {
             return new ArrayTaskList();
         }
-        if (type.equals(ListTypes.types.LINKED)) {
+        if (type == (ListTypes.types.LINKED)) {
             return new LinkedTaskList();
         }
         return null;
