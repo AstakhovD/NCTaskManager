3804024d183b878d8bb70bebe3b0b5d8d0084089
Astakhov Dmitryy <adima4347@gmail.com>
2020-11-13 16:45:53 +0300

    Practice3: LinkedTaskList

--- src/main/java/ua/edu/sumdu/j2se/astakhov/tasks/LinkedTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/astakhov/tasks/LinkedTaskList.java
@@ -58,7 +58,7 @@ public class LinkedTaskList {
 
                     while (current != null) {
                         current = current.next;
-                        index++;
+                        index--;
                     }
                     return true;
                 }
