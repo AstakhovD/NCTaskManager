4c0629f8c8407a473c425ff0f94b7420ee893c69
adima <adima4347@gmail.com>
2020-12-14 17:36:07 +0300

    Practice 7.

--- src/main/java/ua/edu/sumdu/j2se/astakhov/tasks/AbstractTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/astakhov/tasks/AbstractTaskList.java
@@ -1,5 +1,6 @@
 package ua.edu.sumdu.j2se.astakhov.tasks;
 
+import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
 public abstract class AbstractTaskList implements Iterable<Task>, Cloneable {
@@ -14,16 +15,16 @@ public abstract class AbstractTaskList implements Iterable<Task>, Cloneable {
 
     public abstract Stream<Task> getStream();
 
-    public final AbstractTaskList incoming(int from, int to) {
+    public final AbstractTaskList incoming(LocalDateTime from, LocalDateTime to) {
         if(this.getClass().equals(ArrayTaskList.class)) {
             ArrayTaskList arrayTaskList = new ArrayTaskList();
-            this.getStream().filter(task -> task != null && task.nextTimeAfter(from) != -1 && task.getEndTime() <= to)
+            this.getStream().filter(task -> task != null && task.nextTimeAfter(from) != null && task.getEndTime().isBefore(to)) // isBefore - <=
                     .forEach(arrayTaskList :: add);
             return arrayTaskList;
         }
         if (this.getClass().equals(LinkedTaskList.class)) {
             LinkedTaskList linkedTaskList = new LinkedTaskList();
-            this.getStream().filter(task -> task != null && task.nextTimeAfter(from) != -1 && task.getEndTime() <= to)
+            this.getStream().filter(task -> task != null && task.nextTimeAfter(from) != null && task.getEndTime().isBefore(to))
                     .forEach(linkedTaskList :: add);
             return linkedTaskList;
         }
---
+++ src/main/java/ua/edu/sumdu/j2se/astakhov/tasks/Tasks.java
@@ -0,0 +1,38 @@
+package ua.edu.sumdu.j2se.astakhov.tasks;
+
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.*;
+
+public class Tasks {
+    public static Iterable<Task> incoming(Iterable<Task> tasks, LocalDateTime start, LocalDateTime end) {
+        return new Iterable<Task>() {
+            @Override
+            public Iterator<Task> iterator() {
+                Stream<Task> streamTask = StreamSupport.stream(tasks.spliterator(), false);
+                Iterator<Task> iterator = streamTask.filter(task -> task != null && task.nextTimeAfter(start) != null && !task.nextTimeAfter(start).isAfter(end))
+                        .iterator();
+                return iterator;
+            }
+        };
+    }
+
+    public static SortedMap<LocalDateTime, Set<Task>> calendar(Iterable<Task> tasks, LocalDateTime start, LocalDateTime end) {
+        SortedMap<LocalDateTime, Set<Task>> sortedTask = new TreeMap<>();
+        for (Task task : tasks) {
+            LocalDateTime current = task.nextTimeAfter(start.minusSeconds(1));
+            while (current != null && !current.isAfter(end)) {
+                if(sortedTask.containsKey(current)) {
+                    sortedTask.get(current).add(task);
+                }
+                else {
+                    Set<Task> set = new HashSet<>();
+                    set.add(task);
+                    sortedTask.put(current, set);
+                }
+                current = task.nextTimeAfter(current);
+            }
+        }
+        return sortedTask;
+    }
+}
